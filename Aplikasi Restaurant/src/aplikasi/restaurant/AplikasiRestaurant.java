4dd1b20894e101d8ca7f470ae04e705ce1a7dbd4
Aly Rachman Hidayat <alyrahman2003@gmail.com>
2021-02-09 19:58:17 +0700

    Implementasi Struktur kelas aplikasi kasir

---
+++ Aplikasi Restaurant/src/aplikasi/restaurant/AplikasiRestaurant.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi.restaurant;
+
+/**
+ *
+ * @author gin0612
+ */
+public class AplikasiRestaurant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Aplikasi Restaurant/src/classes/DaftarMenu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author gin0612
+ */
+import java.util.ArrayList;
+
+
+public class DaftarMenu {
+    private ArrayList<Menu> daftarMenu;
+    
+    public DaftarMenu() { }
+    
+    public void tambahMenu(Menu menu) { }
+    
+    public void getMenuByKategori(String kategori) { }
+    
+    public void tampilDaftarMenu() { }
+    
+}
