765fdf60e96987901e5e491523a118c9264b7bec
Aly Rachman Hidayat <alyrahman2003@gmail.com>
2021-02-23 21:55:35 +0700

    Add Fitur Menambah dan menampilkan Daftar Menu

--- Aplikasi Restaurant/src/main/MainAplikasikasir.java
+++ Aplikasi Restaurant/src/main/MainAplikasikasir.java
@@ -9,10 +9,43 @@ package main;
  *
  * @author gin0612
  */
+import classes.*;
+import java.util.Scanner;
 public class MainAplikasikasir {
     
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+        //inisialisasi kelas Scanner untuk mengambil
+        //input dari keyboard
+        Scanner input = new Scanner(System.in);
+        
+        MainAplikasikasir app = new MainAplikasikasir();
+        //tampilkan daftar menu
+        app.generateDaftarMenu();
+    }
     
-    public void generateDaftarMenu() { }
+    public void generateDaftarMenu() {
+        daftarMenu = new DaftarMenu();
+        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Original", 18000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Vegetarian", 22000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Karnivor", 28000));
+        daftarMenu.tambahMenu(new Kuah("Kuah Orisinil"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Internasional"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Spicy Lada"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Soto Padang"));
+        daftarMenu.tambahMenu(new Topping("Crab Stick Bakar", 6000));
+        daftarMenu.tambahMenu(new Topping("Chicken Katsu", 8000));
+        daftarMenu.tambahMenu(new Topping("Gyoza Goreng", 4000));
+        daftarMenu.tambahMenu(new Topping("Bakso Goreng", 7000));
+        daftarMenu.tambahMenu(new Topping("Enoki Goreng", 5000));
+        daftarMenu.tambahMenu(new Minuman("Jus Alpukat SCP", 10000));
+        daftarMenu.tambahMenu(new Minuman("Jus Stroberi", 11000));
+        daftarMenu.tambahMenu(new Minuman("Cappucino Coffee", 15000));
+        daftarMenu.tambahMenu(new Minuman("Vietnam Dripp", 14000));
+        
+        daftarMenu.tampilDaftarMenu();
+    }
     
+    public DaftarMenu daftarMenu;
 }
+
